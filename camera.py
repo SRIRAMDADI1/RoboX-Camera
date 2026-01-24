4d056f16357faf5bde7513ed6dfb1316b1ad7306
sd19022007 <sd19022007@gmail.com>
2026-01-24 14:48:18 -0500

    camera mod

--- camera.py
+++ camera.py
@@ -3,9 +3,6 @@ import ctypes
 import cv2
 import numpy as np
 
-
-import os
-
 DLL_DIRS = [
     r"C:\Program Files (x86)\Common Files\MVS\Runtime\Win64_x64",
     r"C:\Program Files\Common Files\MVS\Runtime\Win64_x64",
@@ -20,34 +17,50 @@ os.environ["PATH"] = dll_dir + ";" + os.environ.get("PATH", "")
 
 from MVS.MvCameraControl_class import *
 
-def main():
-    deviceList = MV_CC_DEVICE_INFO_LIST()
-    MvCamera.MV_CC_EnumDevices(MV_USB_DEVICE, deviceList)
-
-    stDevInfo = ctypes.cast(
-        deviceList.pDeviceInfo[0],
-        ctypes.POINTER(MV_CC_DEVICE_INFO)
-    ).contents
-
-    cam = MvCamera()
+def cam_config(cam: MvCamera, stDevInfo):
     cam.MV_CC_CreateHandle(stDevInfo)
     cam.MV_CC_OpenDevice(MV_ACCESS_Exclusive, 0)
+
+    cam.MV_CC_SetEnumValue("PixelFormat", PixelType_Gvsp_RGB8_Packed)
+
+    cam.MV_CC_SetEnumValue("ExposureAuto", 2) 
+    cam.MV_CC_SetEnumValue("GainAuto", 2)  
+
+    cam.MV_CC_SetEnumValue("BalanceWhiteAuto", 2) 
+
+
     cam.MV_CC_StartGrabbing()
 
+def main():
+    deviceList = MV_CC_DEVICE_INFO_LIST()
+    ret = MvCamera.MV_CC_EnumDevices(MV_USB_DEVICE, deviceList)
+    if ret != 0 or deviceList.nDeviceNum == 0:
+        raise RuntimeError("No USB camera found.")
+
+    stDevInfo = ctypes.cast(deviceList.pDeviceInfo[0], ctypes.POINTER(MV_CC_DEVICE_INFO)).contents
+
     stFrameInfo = MV_FRAME_OUT_INFO_EX()
-    buf_size = 50 * 1024 * 1024
-    data_buf = (ctypes.c_ubyte * buf_size)()
+    
+    cam = MvCamera()
+    cam_config(cam, stDevInfo)
+
+    payload = MVCC_INTVALUE()
+    cam.MV_CC_GetIntValue("PayloadSize", payload)
+    payload_size = int(payload.nCurValue)
+
+    data_buf = (ctypes.c_ubyte * payload_size)()
 
     while True:
-        ret = cam.MV_CC_GetOneFrameTimeout(data_buf, buf_size, stFrameInfo, 1000)
+        ret = cam.MV_CC_GetOneFrameTimeout(data_buf, payload_size, stFrameInfo, 1000)
         if ret != 0:
             continue
 
         w, h = stFrameInfo.nWidth, stFrameInfo.nHeight
-        frame = np.frombuffer(data_buf, dtype=np.uint8, count=w * h)
-        img = frame.reshape(h, w)
 
-        cv2.imshow("HIK ROBOT Live", img)
+        img_rgb = np.frombuffer(data_buf, dtype=np.uint8, count=w * h * 3).reshape(h, w, 3)
+        img_bgr = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2BGR)
+
+        cv2.imshow("HIK ROBOT Live (RGB)", img_bgr)
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
 
@@ -55,6 +68,7 @@ def main():
     cam.MV_CC_CloseDevice()
     cam.MV_CC_DestroyHandle()
     cv2.destroyAllWindows()
+
 
 if __name__ == "__main__":
     main()
